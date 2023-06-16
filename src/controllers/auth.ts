e632ac61dc3d5da6fc654d6670b1659e332b7468
Eric Oliveira <ericjmoliveira@gmail.com>
2023-06-16 11:57:56 -0300

    Set up authentication

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,9 +1,13 @@
 import { Request, Response } from 'express';
 import { z } from 'zod';
 import { fromZodError } from 'zod-validation-error';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+import { prisma } from '../lib/prisma';
 
 export const authController = {
-  signIn(request: Request, response: Response) {
+  async signIn(request: Request, response: Response) {
     const body = request.body;
     const schema = z.object({
       email: z
@@ -17,7 +21,23 @@ export const authController = {
     try {
       const data = schema.parse(body);
 
-      return response.status(200).json({ message: 'OK' });
+      const user = await prisma.user.findUnique({ where: { email: data.email } });
+
+      if (!user) {
+        return response.status(404).json({ error: 'The user was not found' });
+      }
+
+      const correctPassword = await bcrypt.compare(data.password, user.password);
+
+      if (!correctPassword) {
+        return response.status(401).json({ error: 'The password is incorrect' });
+      }
+
+      user.password = undefined!;
+
+      const token = jwt.sign(user.id, process.env.JWT_SECRET!);
+
+      return response.status(200).json({ user, token });
     } catch (error) {
       if (error instanceof z.ZodError) {
         const validationError = fromZodError(error);
@@ -29,7 +49,7 @@ export const authController = {
     }
   },
 
-  signUp(request: Request, response: Response) {
+  async signUp(request: Request, response: Response) {
     const body = request.body;
     const schema = z.object({
       firstName: z
@@ -49,7 +69,31 @@ export const authController = {
     try {
       const data = schema.parse(body);
 
-      return response.status(200).json({ message: 'OK' });
+      const hash = await bcrypt.hash(data.password, 10);
+      data.password = hash;
+
+      const userExists = await prisma.user.findUnique({ where: { email: data.email } });
+
+      if (userExists) {
+        return response.status(409).json({ error: 'The user already exists' });
+      }
+
+      const user = await prisma.user.create({
+        data,
+        select: {
+          id: true,
+          firstName: true,
+          lastName: true,
+          email: true,
+          password: false,
+          createdAt: true,
+          updatedAt: true
+        }
+      });
+
+      const token = jwt.sign(user.id, process.env.JWT_SECRET!);
+
+      return response.status(200).json({ user, token });
     } catch (error) {
       if (error instanceof z.ZodError) {
         const validationError = fromZodError(error);
